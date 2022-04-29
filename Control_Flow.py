814e3fe8ea887752efaf776e8e4210c6e98bff95
los Merengue <michaels4uandme@gmail.com>
2022-04-29 17:31:45 +0100

    Create an HTML List

--- Control_Flow.py
+++ Control_Flow.py
@@ -83,3 +83,16 @@ for token in tokens:
         count+=1
 print("The Number of the Elements of the list are {} and the number of XML tag is {}".format(len(tokens),count))
 print(count)
+
+### Create an HTML List
+
+items = ['first string', 'second string', 'third tag', 'fourth tag']
+html_str = "<ul>\n"  # "\ n" is the character that marks the end of the line, it does
+                     # the characters that are after it in html_str are on the next line
+
+# write your code here
+for item in items:
+    dem = "<li>{}</li>\n".format(item)
+    html_str += dem
+html_str += "</ul>"
+print(html_str)
