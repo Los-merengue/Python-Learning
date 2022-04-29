92faf413d32decaccdcaff284f44bf9e1b7f155c
los Merengue <michaels4uandme@gmail.com>
2022-04-29 18:42:06 +0100

    Add a script to print the Color in lowercase

--- Control_Flow.py
+++ Control_Flow.py
@@ -97,3 +97,12 @@ for item in items:
 html_str += "</ul>"
 print(html_str)
 
+### Printing the Color List in lower case
+
+colors = ['Red', 'Blue', 'Green', 'Purple']
+lower_colors = [ ]
+
+for color in colors:
+    #finish this part
+    lower_colors.append(color.lower())
+print(lower_colors)
