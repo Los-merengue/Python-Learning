7a907f93845f4f8abb33740dbe01c744584080fb
los Merengue <michaels4uandme@gmail.com>
2022-04-30 17:57:22 +0100

    Add a word counter

--- Control_Flow.py
+++ Control_Flow.py
@@ -108,3 +108,14 @@ for color in colors:
 print(lower_colors)
 
 print("The lowercase of the string is gien as {}".format(lower_colors))
+
+### Building Dictionaries
+
+book_title =  ['great', 'expectations','the', 'adventures', 'of', 'sherlock','holmes','the','great','gasby','hamlet','adventures','of','huckleberry','fin']
+word_counter = {}
+for word in book_title:
+    if word not in word_counter:
+        word_counter[word] = 1
+    else:
+        word_counter[word] += 1
+print(word_counter)
