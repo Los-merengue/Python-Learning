6b999e688cc87f926201b0235996a260f2f31e43
los Merengue <michaels4uandme@gmail.com>
2022-05-01 23:53:04 +0100

    Modify the script

--- Control_Flow.py
+++ Control_Flow.py
@@ -111,7 +111,7 @@ print("The lowercase of the string is gien as {}".format(lower_colors))
 
 ### Building Dictionaries
 
-book_title =  ['Great', 'Expectations','The', 'adventures', 'of', \
+book_title =  ['Great', 'Expectations','The', 'Adventures', 'of', \
     'sherlock','holmes','the','great','gasby','hamlet','adventures','of','huckleberry','fin']
 word_counter = {}
 for word in book_title:
