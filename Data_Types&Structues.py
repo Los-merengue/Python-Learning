6cb3ad02a1113ae02491745b4f60accfff09ec85
los Merengue <michaels4uandme@gmail.com>
2022-05-06 16:03:33 +0100

    Add a script from my Karat Interview

--- Data_Types&Structues.py
+++ Data_Types&Structues.py
@@ -184,3 +184,77 @@ for key, values in basket_items.items():
         result += int(values)
 
 print(result)
+
+'''
+Our sensor technology has received a major upgrade, and now provides two-dimensional images using nested arrays.
+
+- Every pixel is still a 1 or a 0.
+- The image will contain exactly one rectangle of 0s on a background of 1s.
+- We consider the top left corner to be 0,0
+
+Write a function that takes the image as input and returns the row and column indices of the rectangle's top-left corner.
+
+You can choose to reuse or modify first_zero or get_width, or start over.
+
+For complexity analysis, the dimensions of the input are width(w) and height(h)
+'''
+
+def find_rectangle(image):
+    for i in range(0, len(image)):
+        for j in range(0, len(image[i])):
+            if image[i][j] == 0 :
+                return i,j 
+
+image1 = [
+  [1, 1, 1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1, 1, 1],
+  [1, 1, 1, 0, 0, 0, 1],
+  [1, 1, 1, 0, 0, 0, 1],
+  [1, 1, 1, 1, 1, 1, 1],
+]
+print(find_rectangle(image1)) # (2,3)  row, column
+
+image2 = [
+  [1, 1, 1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1, 1, 0],
+]
+print(find_rectangle(image2)) # (4,6)
+
+image3 = [
+  [1, 1, 1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1, 0, 0],
+  [1, 1, 1, 1, 1, 0, 0],
+]
+print(find_rectangle(image3)) # (3,5)
+  
+image4 = [
+  [0, 1, 1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1, 1, 1],
+]
+print(find_rectangle(image4)) # (0,0)
+
+image5 = [
+  [0],
+]
+print(find_rectangle(image5)) # (0,0)
+
+
+
+def first_zero(image1):
+    for i in range(0,len(image1)):
+        if (image1[i]) == 0:
+            return i
+def get_width(image1):
+    result = 0
+    for i in range(0, len(image1)):
+        if (image1[i]) == 0:
+            result += 1
+    return (result)
