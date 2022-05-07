c8d280e2f5b9d1e445e07c53bf32084fe60efa9b
los Merengue <michaels4uandme@gmail.com>
2022-05-07 22:46:21 +0100

    Add example 2 of the script to the code

--- Data_Types&Structues.py
+++ Data_Types&Structues.py
@@ -272,3 +272,17 @@ for key, values in basket_items.items():
 
 print(result)
 
+#Example 2
+
+result = 0
+basket_items = {'peaches': 5, 'lettuce': 2, 'kites': 3, 'sandwiches': 8, 'pears': 4}
+fruits = ['apples', 'oranges', 'pears', 'peaches', 'grapes', 'bananas']
+
+# Your previous solution here
+for key, values in basket_items.items():
+    if key in fruits:
+        result += int(values)
+
+print(result)
+
+
