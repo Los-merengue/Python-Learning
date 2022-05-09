99aaf117feb5f269b85adb8cd392b520257fce20
los Merengue <michaels4uandme@gmail.com>
2022-05-09 01:57:21 +0100

    Modify the most updated script

--- Data_Types&Structues.py
+++ Data_Types&Structues.py
@@ -322,4 +322,5 @@ for keys, values in basket_items.items():
     if keys not in fruits:
         not_fruit_count += values
 
-print("The number of fruits is {}.  There are {} objects that are not fruits.".format(fruit_count, not_fruit_count))
+print("The number of fruits is {}.  There are {} objects \
+     that are not fruits.".format(fruit_count, not_fruit_count))
