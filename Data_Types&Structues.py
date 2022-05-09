4deb6bca4c3bff617ea1e7fd2366dc0ef08f0ba6
los Merengue <michaels4uandme@gmail.com>
2022-05-09 02:29:08 +0100

    Add a Count By Check Quiz

--- Data_Types&Structues.py
+++ Data_Types&Structues.py
@@ -375,3 +375,21 @@ while break_num < end_num:
     break_num += count_by
 
 print(break_num)
+
+### Count By Check Quiz
+
+start_num = 5
+end_num = 100
+count_by = 2
+
+if start_num > end_num:
+    result = "Oops! Looks like your start value is greater than the end value. Please try again."
+
+else:
+    break_num = start_num
+    while break_num < end_num:
+        break_num += count_by
+
+    result = break_num
+
+print(result)
