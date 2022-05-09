d471e0c3e32c3675b990aace69f66e200b5a7a83
los Merengue <michaels4uandme@gmail.com>
2022-05-09 02:31:01 +0100

    Add a 'Nearest Square' Quiz

--- Data_Types&Structues.py
+++ Data_Types&Structues.py
@@ -393,3 +393,14 @@ else:
     result = break_num
 
 print(result)
+
+### Nearest Square Quiz 
+
+limit = 40
+
+num = 0
+while (num+1)**2 < limit:
+    num += 1
+nearest_square = num**2
+
+print(nearest_square) 
