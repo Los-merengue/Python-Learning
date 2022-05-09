1ba9138644d82def69730c5111e96005b45da86c
los Merengue <michaels4uandme@gmail.com>
2022-05-09 02:00:09 +0100

    Implement a factorial question using for loop

--- Data_Types&Structues.py
+++ Data_Types&Structues.py
@@ -347,3 +347,20 @@ while current <= 6:
 
 # print the factorial of number
 print(product)
+
+### Factorial Question Using For Loop
+
+# number to find the factorial of
+number = 6   
+
+# start with our product equal to one
+product = 1
+
+# write your for loop here
+for i in range(1,7):
+    product = (product * i)
+
+
+
+# print the factorial of number
+print(product)
