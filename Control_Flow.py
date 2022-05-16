259448f236b2bc15dd3293b471bf2c7052aa5072
los Merengue <michaels4uandme@gmail.com>
2022-05-16 13:09:22 +0100

    Modify the script

--- Control_Flow.py
+++ Control_Flow.py
@@ -292,4 +292,3 @@ while (count_odd < 5) and (i < len_num_list):
 
 print ("The numbers of odd numbers added are: {}".format(count_odd))
 print ("The sum of the odd numbers added is: {}".format(list_sum))
-
