28049b917fb3fe23a8eae2c4f3709780e463baa0
los Merengue <michaels4uandme@gmail.com>
2022-05-17 13:12:38 +0100

    Unzip tuples script

--- Control_Flow.py
+++ Control_Flow.py
@@ -406,3 +406,12 @@ cast_heights = [72, 68, 72, 66, 76]
 
 cast = dict(zip(cast_names, cast_heights))
 print(cast)
+
+'''
+Unzip Turples
+'''
+cast = (("Barney", 72), ("Robin", 68), ("Ted", 72), ("Lily", 66), ("Marshall", 76))
+
+names, heights = zip(*cast)
+print(names)
+print(heights)
