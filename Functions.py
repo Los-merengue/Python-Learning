9d0124531be933a0796c77ea06756d61df0198b9
los Merengue <michaels4uandme@gmail.com>
2022-05-17 19:03:37 +0100

    Function that would show plus

---
+++ Functions.py
@@ -0,0 +1,10 @@
+"""
+Print vs. Return in Functions
+Here are two valid functions. One returns a value and one simply 
+prints a value, without returning anything. Test run this code 
+and experiment to understand the difference.
+"""
+
+# this prints something, but does not return anything
+def show_plus_ten(num):
+    print(num + 10)
