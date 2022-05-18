685c4320863b16947fdbcc3097316d2a1a405d0d
los Merengue <michaels4uandme@gmail.com>
2022-05-18 19:22:14 +0100

    Third Docstring Example

--- Functions.py
+++ Functions.py
@@ -82,3 +82,17 @@ def readable_timedelta(days):
     weeks = days // 7
     remainder = days % 7
     return "{} week(s) and {} day(s)".format(weeks, remainder)
+
+def readable_timedelta(days):
+    """
+    Return a string of the number of weeks and days included in days.
+
+    Parameters:
+    days -- number of days to convert (int)
+
+    Returns:
+    string of the number of weeks and days included in days
+    """
+    weeks = days // 7
+    remainder = days % 7
+    return "{} week(s) and {} day(s)".format(weeks, remainder)
