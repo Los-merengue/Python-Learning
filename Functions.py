039efabd66d8f772107aafaa7aaf757998c72b03
los Merengue <michaels4uandme@gmail.com>
2022-05-18 19:28:35 +0100

    Lambda funtions with compound functions

--- Functions.py
+++ Functions.py
@@ -103,3 +103,17 @@ Writing Lambda i.e. Functions that dont have a name
 multiply = lambda x, y: x * y
 
 multiply(10,7)
+
+'''
+Using Lambda function with Compound Functions
+'''
+# Lambda with Map
+numbers = [
+              [34, 63, 88, 71, 29],
+              [90, 78, 51, 27, 45],
+              [63, 37, 85, 46, 22],
+              [51, 22, 34, 11, 18]
+           ]
+
+averages = list(map(lambda num_list: sum(num_list) / len(num_list), numbers))
+print(averages)
