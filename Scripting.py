c25816e578bbe239ead5dfde3c02e729367d630f
los Merengue <michaels4uandme@gmail.com>
2022-05-20 16:15:07 +0100

    Add a script for cookies and left_overs

--- Scripting.py
+++ Scripting.py
@@ -27,3 +27,28 @@ to {} if you submit all assignments before the due date.\n\n"
 
 for name, assignment, grade in zip(names, assignments, grades):
     print(message.format(name, assignment, grade, int(grade) + int(assignment)*2))
+
+def party_planner(cookies, people):
+    leftovers = None
+    num_each = None
+    # TODO: Add a try-except block here to
+    #       make sure no ZeroDivisionError occurs.
+    num_each = cookies // people
+    leftovers = cookies % people
+
+    return(num_each, leftovers)
+
+# The main code block is below; do not edit this
+lets_party = 'y'
+while lets_party == 'y':
+
+    cookies = int(input("How many cookies are you baking? "))
+    people = int(input("How many people are attending? "))
+
+    cookies_each, leftovers = party_planner(cookies, people)
+
+    if cookies_each:  # if cookies_each is not None
+        message = "\nLet's party! We'll have {} people attending, they'll each get to eat {} cookies, and we'll have {} left over."
+        print(message.format(people, cookies_each, leftovers))
+
+    lets_party = input("\nWould you like to party more? (y or n) ")
