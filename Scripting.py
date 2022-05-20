b815e6553d83b4af5aa4b67b6a5b69fd0c0f546c
los Merengue <michaels4uandme@gmail.com>
2022-05-20 13:12:27 +0100

    Init Scripting file

---
+++ Scripting.py
@@ -0,0 +1,13 @@
+how_many_snakes = 1
+snake_string = """
+Welcome to Python3!
+
+             ____
+            / . .\\
+            \  ---<
+             \  /
+   __________/ /
+-=:___________/
+
+<3, Juno
+"""
