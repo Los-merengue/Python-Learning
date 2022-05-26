83e0e7b22d864af08a92dacf9391b818dbc2420b
los Merengue <michaels4uandme@gmail.com>
2022-05-26 17:16:12 +0100

    Create a list in the function

--- Advanced Topic.py
+++ Advanced Topic.py
@@ -1,4 +1,5 @@
-lessons = ["Why Python Programming", "Data Types and Operators", "Control Flow", "Functions", "Scripting"]
+lessons = ["Why Python Programming", "Data Types and Operators",
+ "Control Flow", "Functions", "Scripting"]
 
 for i, lesson in my_enumerate(lessons, 1):
     print("Lesson {}: {}".format(i, lesson))
