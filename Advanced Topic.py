f074f2e2bbe6cd4f179e5fa0ea60c4230268389f
los Merengue <michaels4uandme@gmail.com>
2022-05-26 17:13:56 +0100

    Created an Iterator file

---
+++ Advanced Topic.py
@@ -0,0 +1,4 @@
+lessons = ["Why Python Programming", "Data Types and Operators", "Control Flow", "Functions", "Scripting"]
+
+for i, lesson in my_enumerate(lessons, 1):
+    print("Lesson {}: {}".format(i, lesson))
