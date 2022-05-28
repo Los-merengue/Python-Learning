dabb228a60cb45dfe13dc53499d5d34cea8e1efd
los Merengue <michaels4uandme@gmail.com>
2022-05-28 21:17:20 +0100

    Modify the list in the program

--- Advanced Topic.py
+++ Advanced Topic.py
@@ -1,5 +1,4 @@
-lessons = ["Why Python Programming", "Data Types and Operators",
-"Control Flow", "Functions", "Scripting"]
+lessons = ["Why Python Programming", "Data Types and Operators","Control Flow", "Functions", "Scripting"]
 
 for i, lesson in enumerate(lessons, 1):
     print("Lesson {}: {}".format(i, lesson))
